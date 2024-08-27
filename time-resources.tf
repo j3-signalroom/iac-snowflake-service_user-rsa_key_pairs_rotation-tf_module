3d5f4e88b1e7ebfc3487059af1b4d63b9852b1e0
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2024-08-27 15:43:05 -0400

    See #1. Initial commit.

---
+++ time-resources.tf
@@ -0,0 +1,15 @@
+
+# This controls each key's rotations
+resource "time_rotating" "rsa_key_pair_rotations" {
+    count = number_of_rsa_key_pairs_to_retain
+    rotation_days = var.day_count*number_of_rsa_key_pairs_to_retain
+    rfc3339 = timeadd(local.now, format("-%sh", (count.index)*local.hour_count))
+}
+
+# Store the retain RSA key pairs and the time when the rotation needs to accord.  In order, to 
+# trigger a `replace_triggered_by` on the RSA key pair.  Refer to GitHub Issue for more info
+# https://github.com/hashicorp/terraform-provider-time/issues/118
+resource "time_static" "rsa_key_pair_rotations" {
+    count = local.number_of_rsa_key_pairs_to_retain
+    rfc3339 = time_rotating.rsa_key_pair_rotations[count.index].rfc3339
+}
