d540c1e5e5793537fe7c503926f5baff4200c0e8
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-07-21 10:34:05 -0400

    Resolved #165.

--- setup-aws-secretsmanager.tf
+++ setup-aws-secretsmanager.tf
@@ -2,7 +2,7 @@
 # And, initially store placeholder values in the secrets, which will later be filled in by the
 # Lambda function
 resource "aws_secretsmanager_secret" "public_keys" {
-    name = var.secret_insert == "" ? "/snowflake_resource" : "/snowflake_resource/${var.secret_insert}"
+    name = local.base_secrets_path
 }
 
 resource "aws_secretsmanager_secret_version" "public_keys" {
@@ -16,7 +16,7 @@ resource "aws_secretsmanager_secret_version" "public_keys" {
 # Create the Private RSA key pair 1 secret in AWS Secrets Manager.  And, initially store a placeholder
 # value in the secret, which will later be filled in by the Lambda function
 resource "aws_secretsmanager_secret" "private_key_pem_1" {
-    name = var.secret_insert == "" ? "/snowflake_resource/rsa_private_key_pem_1" : "/snowflake_resource/${var.secret_insert}/rsa_private_key_pem_1"
+    name = "${local.base_secrets_path}/rsa_private_key_pem_1"
 }
 
 resource "aws_secretsmanager_secret_version" "private_key_pem_1" {
@@ -27,7 +27,7 @@ resource "aws_secretsmanager_secret_version" "private_key_pem_1" {
 # Create the Private RSA key pair 2 secret in AWS Secrets Manager.  And, initially store a placeholder
 # value in the secret, which will later be filled in by the Lambda function
 resource "aws_secretsmanager_secret" "private_key_pem_2" {
-    name = var.secret_insert == "" ? "/snowflake_resource/rsa_private_key_pem_2" : "/snowflake_resource/${var.secret_insert}/rsa_private_key_pem_2"
+    name = "${local.base_secrets_path}/rsa_private_key_pem_2"
 }
 
 resource "aws_secretsmanager_secret_version" "private_key_pem_2" {
@@ -35,11 +35,10 @@ resource "aws_secretsmanager_secret_version" "private_key_pem_2" {
     secret_string = "<RSA_PRIVATE_KEY_PEM_2>"
 }
 
-
 # Create the Private RSA key pair 1 secret in AWS Secrets Manager.  And, initially store a placeholder
 # value in the secret, which will later be filled in by the Lambda function
 resource "aws_secretsmanager_secret" "private_key_1" {
-    name = var.secret_insert == "" ? "/snowflake_resource/rsa_private_key_1" : "/snowflake_resource/${var.secret_insert}/rsa_private_key_1"
+    name = "${local.base_secrets_path}/rsa_private_key_1"
 }
 
 resource "aws_secretsmanager_secret_version" "private_key_1" {
@@ -50,7 +49,7 @@ resource "aws_secretsmanager_secret_version" "private_key_1" {
 # Create the Private RSA key pair 2 secret in AWS Secrets Manager.  And, initially store a placeholder
 # value in the secret, which will later be filled in by the Lambda function
 resource "aws_secretsmanager_secret" "private_key_2" {
-    name = var.secret_insert == "" ? "/snowflake_resource/rsa_private_key_2" : "/snowflake_resource/${var.secret_insert}/rsa_private_key_2"
+    name = "${local.base_secrets_path}/rsa_private_key_2"
 }
 
 resource "aws_secretsmanager_secret_version" "private_key_2" {
