a15c3ade6ad8e71a459b822dd4149b928cc7975f
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-07-23 15:18:27 -0400

    Resolved #177.

--- data.tf
+++ data.tf
@@ -2,20 +2,19 @@ data "aws_caller_identity" "current" {}
 
 locals {
     # Repo name and URIs
-    repo_name    = "iac-snowflake-user-rsa_key_pairs_and_jwt_generator"
-    repo_uri     = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${local.repo_name}:latest"
-    ecr_repo_uri = "arn:aws:ecr:${var.aws_region}:${data.aws_caller_identity.current.account_id}:repository/${local.repo_name}"
-
-    now        = timestamp()
-    hour_count = var.day_count*24
+    repo_name           = "iac-snowflake-user-rsa_key_pairs_and_jwt_generator"
+    repo_uri            = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${local.repo_name}:latest"
+    ecr_repo_uri        = "arn:aws:ecr:${var.aws_region}:${data.aws_caller_identity.current.account_id}:repository/${local.repo_name}"
+    
+    now                 = timestamp()
+    hour_count          = var.day_count*24
 
     # As of 2025-07-23, Snowflake only allows a max of two RSA key pairs that can be rotated for a given user
-    number_of_rsa_key_pairs_to_retain = 2
+    key_pairs_to_retain = 2
 
-    sorted_dates                 = sort(time_rotating.rsa_key_pair_rotations.*.rfc3339)
-    dates_and_count              = zipmap(time_rotating.rsa_key_pair_rotations.*.rfc3339, range(local.number_of_rsa_key_pairs_to_retain))
-    latest_rsa_public_key_number = lookup(local.dates_and_count, local.sorted_dates[0])
-    lambda_response              = jsondecode(aws_lambda_invocation.lambda_function.result)
-    response_body                = jsondecode(local.lambda_response.body)
-    key_pairs                    = local.response_body.secrets
+    sorted_dates        = sort(time_rotating.rsa_key_pair_rotations.*.rfc3339)
+    dates_and_count     = zipmap(time_rotating.rsa_key_pair_rotations.*.rfc3339, range(local.key_pairs_to_retain))
+    key_number          = lookup(local.dates_and_count, local.sorted_dates[0])
+    lambda_response     = jsondecode(aws_lambda_invocation.lambda_function.result)
+    key_pairs           = jsondecode(local.lambda_response.body)
 }
